315a707ec4d4f47ae1816a3f50419bba88f8b35c
Kharron Reid <kharron@senorcoders.com>
2020-02-03 11:28:38 -0800

    Fixed check on video length

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -55,7 +55,7 @@ module.exports = {
 
         if (video.length > 0){
             console.log("Video Info: ", video.video);
-            if (video.video) {
+            if (video.video.length > 0) {
                 console.log("Video Available Sending: ", video.video)
                 res.status(200).send(video);
             } else {
